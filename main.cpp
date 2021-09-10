63bb10fc2f7a5ef08b8a4811395e9bd6a4b8908f
20181185신준원 <83389447+G00dog@users.noreply.github.com>
2021-09-10 03:04:02 +0000

    HelloSDL 랜덤색상

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
+//randomcolor라는 함수에서 난수를 생성해 1초마다 색상이 변하게 했습니다.
+
 #include <SDL.h>
+#include <stdlib.h>
 
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
@@ -30,6 +33,12 @@ void render()
   SDL_RenderPresent(g_pRenderer);
 }
 
+void randomcolor()//1초마다 랜덤한 색깔로 바뀌게 함
+{
+  SDL_SetRenderDrawColor(g_pRenderer, rand()%255, rand()%255, rand()%255, 255);
+  SDL_Delay(1000);
+}
+
 int main(int argc, char* argv[])
 {
   if(init("Breaking Up HelloSDL",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN))
@@ -43,6 +52,7 @@ int main(int argc, char* argv[])
 
   while(g_bRunning)
   {
+    randomcolor();
     render();
   }
 
