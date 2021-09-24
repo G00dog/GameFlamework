2dc6bedfbfc3e6a87b6ec8daa1218acafe3455e2
20181185신준원 <83389447+G00dog@users.noreply.github.com>
2021-09-24 02:16:07 +0000

    원본상자/대상상자 응용 1

--- Game.cpp
+++ Game.cpp
@@ -39,12 +39,17 @@ bool Game::init(const char* title, int xpos, int ypos, int height, int width, in
   SDL_FreeSurface(pTempSurface);
 
   SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);//원본상자 너비, 넓이 설정
+  m_sourceRectangle.x = 0;
+  m_sourceRectangle.y = 0;
+
+  m_sourceRectangle.w = 50;
+  m_sourceRectangle.h = 50;
 
   m_destinationRectangle.w = m_sourceRectangle.w;
   m_destinationRectangle.h = m_sourceRectangle.h;
 
-  m_destinationRectangle.x =m_sourceRectangle.x = 0;
-  m_destinationRectangle.y =m_sourceRectangle.y = 0;
+  m_destinationRectangle.x = 0;
+  m_destinationRectangle.y = 0;
 
   return true;
 }
