cc31063ef65847c1852aa2177e573aa0d20faaa3
20181185신준원 <83389447+G00dog@users.noreply.github.com>
2021-10-29 13:23:43 +0000

    Vector 오류메시지는 없앤것 아래것이 교수님 파일과 같음

--- SDLGameObject.h
+++ SDLGameObject.h
@@ -8,12 +8,15 @@ class SDLGameObject : public GameObject
 public:
   SDLGameObject(const LoaderParams* pParams);
   virtual void draw();
-  virtual void update() {}
+  virtual void update();// 원래 update() {}임 오류떠서 임시수정
   virtual void clean() {}
   virtual ~SDLGameObject() {}
 
 protected:
   Vector2D m_position;
+
+  int m_x;//오류떠서 추가해놓음 지워야함
+  int m_y;//위와 같음
 
   int m_width;
   int m_height;
