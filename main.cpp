d825277da723bcb7afe08de19dd0f3690a27799b
20181185신준원 <83389447+G00dog@users.noreply.github.com>
2021-11-05 03:05:06 +0000

    물리 실습3

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 //완료된 실습
-//상속, 다향성, 유일객체 Game, 추상 Class
+//상속, 다향성, 유일객체 Game, 추상 Class, Vector2D, 물리
 
 #include "Game.h"
 #include <iostream>
 
 Game* g_game = 0;
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
 
 int main(int argc, char* argv[])
 {
   if(TheGame::Instance()->init("Chapter 1", 100, 100, 640, 480, false))
   {
+    Uint32 frameStart, frameTime;
     while(TheGame::Instance()->running())
     {
+      frameStart = SDL_GetTicks();
       TheGame::Instance()->handleEvents();
       TheGame::Instance()->update();
       TheGame::Instance()->render();
-      SDL_Delay(10);
+      frameTime = SDL_GetTicks() - frameStart;
+      if(frameTime < DELAY_TIME)
+      {
+        SDL_Delay((int)(DELAY_TIME - frameTime));
+      }
     }
   }
   else
